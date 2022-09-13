d7b76c6b6eca4f77b99969e0b05ac9a37371a21b
HectorBergman <113025156+HectorBergman@users.noreply.github.com>
2022-09-13 14:20:08 +0200

    Initiala filer för MakeCode-projekt

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Öppna denna sida på [https://hectorbergman.github.io/uppochner20145t4/](https://hectorbergman.github.io/uppochner20145t4/)
+
+## Använd som tillägg
+
+Denna databas kan läggas till som ett **tillägg** i MakeCode.
+
+* öppna [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klicka på **Nytt projekt**
+* klicka på **Tillägg** under kugghjulsmenyn
+* sök efter **https://github.com/hectorbergman/uppochner20145t4** och importera
+
+## Redigera detta projekt ![Byggets statusikon](https://github.com/hectorbergman/uppochner20145t4/workflows/MakeCode/badge.svg)
+
+Redigera den här databasen i MakeCode.
+
+* öppna [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klicka på **Importera** och klicka sedan på **Importera URL**
+* klistra in **https://github.com/hectorbergman/uppochner20145t4** och klicka på importera
+
+## Förhandsgranskning av block
+
+Denna bild visar blockkoden från den senaste incheckningen i master.
+Denna bild kan ta några minuter att ladda om.
+
+![En renderad vy av blocken](https://github.com/hectorbergman/uppochner20145t4/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (används för sökning och rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,133 @@
+function position (x: number, y: number) {
+    if (xPosition - 6 == cubeX && yPosition - 5 == cubeY) {
+        led.unplot(xPosition - 5, yPosition - 5)
+        xPosition = x
+        if (xPosition - 5 == cubeX) {
+            cubeX += -1
+            radio.sendValue("cubeX", cubeX)
+            radio.sendValue("cubeY", cubeY)
+        }
+        yPosition = y
+    } else if (xPosition - 4 == cubeX && yPosition - 5 == cubeY) {
+        led.unplot(xPosition - 5, yPosition - 5)
+        xPosition = x
+        if (xPosition - 5 == cubeX) {
+            cubeX += 1
+            radio.sendValue("cubeX", cubeX)
+            radio.sendValue("cubeY", cubeY)
+        }
+        yPosition = y
+    } else {
+        led.unplot(xPosition - 5, yPosition - 5)
+        xPosition = x
+        yPosition = y
+    }
+}
+function cubeGravityDown (num: number) {
+    for (let index = 0; index < num; index++) {
+        basic.pause(250)
+        led.unplot(cubeX, cubeY)
+        cubeY += 1
+    }
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "xPos") {
+        position(value, yPosition)
+    } else if (name == "yPos") {
+        position(xPosition, value)
+    } else {
+    	
+    }
+})
+function cubeGravityUp (num: number) {
+    for (let index = 0; index < num; index++) {
+        basic.pause(250)
+        led.unplot(cubeX, cubeY)
+        cubeY += -1
+    }
+}
+let otherway = 0
+let compassheading = 0
+let cubeX = 0
+let cubeY = 0
+let yPosition = 0
+let xPosition = 0
+radio.setGroup(1)
+xPosition = 0
+yPosition = 0
+let Initialheading = input.compassHeading()
+let Conestart = (Initialheading + 135) % 360
+let Coneend = (Initialheading + 225) % 360
+let Coneend2 = (Coneend + 180) % 360
+let Conestart2 = (Conestart + 180) % 360
+cubeY = 4
+cubeX = 1
+radio.sendValue("cubeX", cubeX)
+radio.sendValue("cubeY", cubeY)
+basic.forever(function () {
+    compassheading = input.compassHeading()
+})
+basic.forever(function () {
+    if (otherway == 0) {
+        if (Conestart < Coneend) {
+            if (compassheading > Conestart && compassheading < Coneend) {
+                cubeGravityUp(4)
+                otherway = 1
+            } else {
+            	
+            }
+        } else {
+            if (compassheading > Conestart || compassheading < Coneend) {
+                cubeGravityUp(4)
+                otherway = 1
+            } else {
+            	
+            }
+        }
+    } else {
+        if (Conestart2 < Coneend2) {
+            if (compassheading > Conestart2 && compassheading < Coneend2) {
+                cubeGravityDown(4)
+                otherway = 0
+            } else {
+            	
+            }
+        } else {
+            if (compassheading > Conestart2 || compassheading < Coneend2) {
+                cubeGravityDown(4)
+                otherway = 0
+            } else {
+            	
+            }
+        }
+    }
+})
+basic.forever(function () {
+    led.plot(xPosition - 5, yPosition - 5)
+})
+basic.forever(function () {
+    led.plot(4, 0)
+    basic.pause(700)
+    led.unplot(4, 0)
+    basic.pause(1400)
+    led.plot(4, 0)
+})
+basic.forever(function () {
+    led.plot(cubeX, cubeY)
+})
+basic.forever(function () {
+    if (cubeX >= 5) {
+        cubeX = 4
+    }
+    if (cubeY >= 5) {
+        cubeY = 4
+    }
+})
+basic.forever(function () {
+    if (cubeX <= -1) {
+        cubeX = 0
+    }
+    if (cubeY <= -1) {
+        cubeY = 0
+    }
+})
